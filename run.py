5eeff7fadc831e095b69d36248049e14f0ae4eb4
olegsher <github@sher.biz>
2019-12-18 16:45:37 +0200

    remove static and templates

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__,
-            static_folder = "../frontend",
-            template_folder = "../frontend")
+            static_folder = "../frontend/static",
+            template_folder = "../frontend/temptates")
 @app.route('/')
 def index():
     return render_template("index.html")
